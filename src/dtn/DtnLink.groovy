594178a8de12f2c10a7e1b2190c5bb938b3dcdf0
arnav dhamija <arnav.dhamija@gmail.com>
2019-01-29 18:33:33 +0800

    Writing DTNLink

--- src/dtn/DtnLink.groovy
+++ src/dtn/DtnLink.groovy
@@ -4,26 +4,31 @@ import groovy.transform.CompileStatic
 import groovy.transform.TypeChecked
 import org.arl.fjage.AgentID
 import org.arl.fjage.Message
+import org.arl.fjage.Performative
 import org.arl.fjage.TickerBehavior
+import org.arl.unet.Address
+import org.arl.unet.CapabilityReq
 import org.arl.unet.DatagramCapability
+import org.arl.unet.DatagramReq
 import org.arl.unet.Services
 import org.arl.unet.UnetAgent
+import org.arl.unet.nodeinfo.NodeInfoParam
+import org.arl.unet.phy.Physical
 
 @TypeChecked
 @CompileStatic
 class DtnLink extends UnetAgent {
     private DtnStorage storage
     private int nodeAddress
-    private long linkLastSeen
+    private long lastReceivedTime
 
     private AgentID link
     private AgentID notify
+    private AgentID nodeInfo
+    private AgentID phy
 
-    int BEACON_DURATION = 100
-    int SWEEP_DURATION = 100
-
-    TickerBehavior beacon
-    TickerBehavior sweepStorage
+    int BEACON_DURATION = 100*1000
+    int SWEEP_DURATION = 100*1000
 
     @Override
     protected void setup() {
@@ -35,6 +40,50 @@ class DtnLink extends UnetAgent {
 
     @Override
     protected void startup() {
+        nodeInfo = agentForService(Services.NODE_INFO)
+        nodeAddress = (int)get(nodeInfo, NodeInfoParam.address)
+        notify = topic()
+
+        storage = new DtnStorage(Integer.toString(nodeAddress))
+
+        link = getLinkWithReliability()
+        subscribe(link)
+        phy = agent((String)get(link, org.arl.unet.link.ReliableLinkParam.phy))
+        if (phy != null) {
+            subscribe(phy)
+        } else {
+            println "PHY not provided for link"
+        }
+//        add(new TickerBehavior(BEACON_DURATION) {
+//            @Override
+//            void onTick() {
+//                super.onTick()
+//                if (System.currentTimeSeconds() - lastReceivedTime >= BEACON_DURATION) {
+//                    link.send(new DatagramReq(to: Address.BROADCAST))
+//                    lastReceivedTime = System.currentTimeSeconds()
+//                }
+//            }
+//        })
+//
+//        add(new TickerBehavior(SWEEP_DURATION) {
+//            @Override
+//            void onTick() {
+//                super.onTick()
+//                ArrayList<Tuple2> expiredDatagrams = storage.deleteExpiredDatagrams()
+//            }
+//        })
+    }
+
+    AgentID getLinkWithReliability() {
+        AgentID[] links = agentsForService(Services.LINK)
+
+        for (AgentID link : links) {
+            CapabilityReq req = new CapabilityReq(link, DatagramCapability.RELIABILITY)
+            Message rsp = request(req, 500)
+            if (rsp.getPerformative() == Performative.CONFIRM) {
+                return link
+            }
+        }
     }
 
     @Override
