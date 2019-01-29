9c8957a0223266762db2d5f6f7cb9b466a4c486d
arnav dhamija <arnav.dhamija@gmail.com>
2019-01-29 16:34:51 +0800

    added classes

--- src/dtn/DtnLink.groovy
+++ src/dtn/DtnLink.groovy
@@ -2,9 +2,41 @@ package dtn
 
 import groovy.transform.CompileStatic
 import groovy.transform.TypeChecked
+import org.arl.fjage.AgentID
+import org.arl.fjage.Message
+import org.arl.fjage.TickerBehavior
+import org.arl.unet.UnetAgent
+import sun.management.Agent
 
 @TypeChecked
 @CompileStatic
-class DtnLink {
+class DtnLink extends UnetAgent {
+    private DtnStorage storage
+    private int nodeAddress
+    private long linkLastSeen
 
+    private AgentID link
+    private AgentID notify
+
+    int BEACON_DURATION = 100
+    int SWEEP_DURATION = 100
+
+    TickerBehavior beacon
+    TickerBehavior sweepStorage
+
+    @Override
+    protected void setup() {
+    }
+
+    @Override
+    protected void startup() {
+    }
+
+    @Override
+    protected Message processRequest(Message msg) {
+    }
+
+    @Override
+    protected void processMessage(Message msg) {
+    }
 }
