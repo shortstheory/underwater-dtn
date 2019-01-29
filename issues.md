df2dfecbabef241fb0710fb3306dcee7e3656b73
arnav dhamija <arnav.dhamija@gmail.com>
2019-01-29 19:11:22 +0800

    Added issues file

---
+++ issues.md
@@ -0,0 +1,1 @@
+* @TypeChecked breaks TickerBehavior
---
+++ src/dtn/DtnLinkParameters.groovy
@@ -0,0 +1,13 @@
+package dtn
+
+import groovy.transform.CompileStatic
+import groovy.transform.TypeChecked
+import org.arl.unet.Parameter;
+
+@com.google.gson.annotations.JsonAdapter(org.arl.unet.JsonTypeAdapter.class)
+@TypeChecked
+@CompileStatic
+enum DtnLinkParameters implements Parameter {
+    BEACON_DURATION,
+    SWEEP_DURATION
+}
