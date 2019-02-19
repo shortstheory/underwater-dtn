77c7d9da292155530f902cfe490aa3446a831259
arnav dhamija <arnav.dhamija@gmail.com>
2019-02-19 12:24:30 +0800

    Collecting stats

--- src/auv_scenario.groovy
+++ src/auv_scenario.groovy
@@ -26,15 +26,15 @@ def dist = 1000.m
 def msgTtl = 600
 
 int[] dest1 = [2,3]
-int[] dest2 = [3,1]
-int[] dest3 = [2,1]
+int[] dest2 = [1]
+int[] dest3 = [1]
 
 
 for (int f = 0; f < nodeCount; f++) {
     FileUtils.deleteDirectory(new File(Integer.toString(f)))
     Files.deleteIfExists((new File(Integer.toString(f)+".json")).toPath())
 }
-for (int i = 1; i < 2; i++) {
+for (int i = 1; i <= 10; i++) {
     msgSize = i*100
     println("\n===========\nSize - " + msgSize + " Freq - " + msgFreq + " Dist - " + dist + " TTL - " + msgTtl)
     simulate T, {
@@ -43,7 +43,7 @@ for (int i = 1; i < 2; i++) {
             container.add 'dtnlink', new DtnLink(Integer.toString(1))
             container.add 'testagent', new DatagramGenerator(dest1, msgFreq, msgSize, msgTtl)
         }
-        def auvR = node '2', address: 2, mobility: true, location: [1000.m, 0, -50.m], shell: 5001, stack: { container ->
+        def auvR = node '2', address: 2, mobility: true, location: [2400.m, 0, -50.m], shell: 5001, stack: { container ->
             container.add 'link', new ReliableLink()
             container.add 'dtnlink', new DtnLink(Integer.toString(2))
             container.add 'testagent', new DatagramGenerator(dest2, msgFreq, msgSize, msgTtl)
@@ -53,7 +53,7 @@ for (int i = 1; i < 2; i++) {
                            [duration: 600.seconds, heading: 180.deg, speed: 1.mps],
                            [duration: 2000.seconds, heading: 90.deg, speed: 1.mps],
                            [duration: 300.seconds, heading: 0.deg, speed: 1.mps]]
-        def auvL = node '3', address: 3, mobility: true, location: [-1000.m, 0, -50.m], shell: 5001, stack: { container ->
+        def auvL = node '3', address: 3, mobility: true, location: [-2400.m, 0, -50.m], shell: 5001, stack: { container ->
             container.add 'link', new ReliableLink()
             container.add 'dtnlink', new DtnLink(Integer.toString(3))
             container.add 'testagent', new DatagramGenerator(dest3, msgFreq, msgSize, msgTtl)
