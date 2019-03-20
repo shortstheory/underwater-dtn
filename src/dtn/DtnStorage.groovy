b4f926bcd993aa4bee33188fab314b5b18c3e85f
arnav dhamija <arnav.dhamija@gmail.com>
2019-03-20 15:22:42 +0800

    Put changes back -> DtnStorage

--- src/dtn/DtnStorage.groovy
+++ src/dtn/DtnStorage.groovy
@@ -225,11 +225,11 @@ class DtnStorage {
 
     OutputPDU encodePdu(byte[] data, int ttl, int protocol, boolean tbc, int payloadID, int startPtr) {
         int dataLength = (data == null) ? 0 : data.length
-        OutputPDU pdu = new OutputPDU(dataLength + dtnLink.HEADER_SIZE)
+        OutputPDU pdu = new OutputPDU(dataLength + DtnLink.HEADER_SIZE)
         pdu.write24(ttl)
         pdu.write8(protocol)
         int payloadFields
-        payloadFields = (tbc) ? (1 << 32) : 0
+        payloadFields = (tbc) ? (1 << 31) : 0
         payloadFields |= (payloadID << 23)
         payloadFields |= startPtr
         pdu.write32(payloadFields)
@@ -258,15 +258,15 @@ class DtnStorage {
         }
         InputPDU pdu = new InputPDU(pduBytes)
         HashMap<String, Integer> map = new HashMap<>()
-        map.put(TTL_MAP, (int)pdu.read24())
-        map.put(PROTOCOL_MAP, (int)pdu.read8())
+        map.put(DtnStorage.TTL_MAP, (int)pdu.read24())
+        map.put(DtnStorage.PROTOCOL_MAP, (int)pdu.read8())
         int payloadFields = (int)pdu.read32()
-        int tbc = ((payloadFields & 0x80000000).toInteger() >> 31)
-        int payloadID = ((payloadFields & 0x7F800000) >> 19)
-        int startPtr = (payloadFields & 0xFFFFFF)
-        map.put(TBC_BIT_MAP, tbc)
-        map.put(PAYLOAD_ID_MAP, payloadID)
-        map.put(START_PTR_MAP, startPtr)
+        int tbc = ((payloadFields & 0x80000000).toInteger() >>> 31)
+        int payloadID = ((payloadFields & 0x7F800000) >>> 23)
+        int startPtr = (payloadFields & 0x007FFFFF)
+        map.put(DtnStorage.TBC_BIT_MAP, tbc)
+        map.put(DtnStorage.PAYLOAD_ID_MAP, payloadID)
+        map.put(DtnStorage.START_PTR_MAP, startPtr)
         return map
     }
 
